54cd7625f46932be276178911857017683f57f96
Nanlung <nanlung.temlong@gmail.com>
2021-11-18 13:54:31 +0100

    ctx add

--- src/components/Layout/Header/HeaderCartButton.js
+++ src/components/Layout/Header/HeaderCartButton.js
@@ -1,14 +1,21 @@
+import { useContext } from "react";
 import CartIcon from "../../Cart/CartIcon";
+import CardContext from "../../../store/cart-context";
 import classes from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = (props) => {
+  const ctx = useContext(CardContext);
+
+  const numberOfCartItems = ctx.items.reduce((currentNumber, item) => {
+    return currentNumber + item.amount;
+  }, 0);
   return (
     <button className={classes.button} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>3</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
     </button>
   );
 };
