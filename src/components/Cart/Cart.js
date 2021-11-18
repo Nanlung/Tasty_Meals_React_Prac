5422664d3ce662067d6e5a030b65cf041cad6045
Nanlung <nanlung.temlong@gmail.com>
2021-11-18 11:33:41 +0100

    cart feature

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,29 @@
+import classes from "./Cart.module.css";
+
+const Cart = (props) => {
+  const cartItems = (
+    <ul className={classes["cart-items"]}>
+      {[{ id: "c1", name: "Chicken", amount: 4, price: 10.99 }].map(
+        (cartItem) => (
+          <li>{cartItem.name}</li>
+        )
+      )}
+    </ul>
+  );
+
+  return (
+    <div>
+      <div className={classes.total}>
+        {cartItems}
+        <span>Total Amount</span>
+        <span>40.00</span>
+      </div>
+      <div className={classes.actions}>
+        <button className={classes["button--alt"]}>Close</button>
+        <button className={classes.button}>Order</button>
+      </div>
+    </div>
+  );
+};
+
+export default Cart;
