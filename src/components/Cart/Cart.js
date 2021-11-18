3184440ee501f9b2c31891a0d8abe62faca6c64a
Nanlung <nanlung.temlong@gmail.com>
2021-11-18 13:08:22 +0100

    event feature

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -13,14 +13,16 @@ const Cart = (props) => {
   );
 
   return (
-    <Modal>
+    <Modal onClose={props.onClose}>
       <div className={classes.total}>
         {cartItems}
         <span>Total Amount</span>
         <span>40.00</span>
       </div>
       <div className={classes.actions}>
-        <button className={classes["button--alt"]}>Close</button>
+        <button className={classes["button--alt"]} onClick={props.onClose}>
+          Close
+        </button>
         <button className={classes.button}>Order</button>
       </div>
     </Modal>
--- src/components/Layout/Header/Header.js
+++ src/components/Layout/Header/Header.js
@@ -2,12 +2,12 @@ import mealsImage from "../../../assets/meals.jpg";
 import HeaderCartButton from "./HeaderCartButton";
 import classes from "./Header.module.css";
 
-const Header = () => {
+const Header = (props) => {
   return (
     <>
       <header className={classes.header}>
         <h1>Tasty Meals</h1>
-        <HeaderCartButton />
+        <HeaderCartButton onClick={props.onShowCart} />
       </header>
       <div className={classes["main-image"]}>
         <img src={mealsImage} alt="Buffet" />
